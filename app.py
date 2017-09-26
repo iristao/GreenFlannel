234e5bdb8af6a292a52c44c032672a1bac4004fa
Manahal Tabassum <mtabassum@stuy.edu>
2017-09-26 18:20:15 -0400

    Finished setting up the table

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
+import csv
+import random
+occupations_dict = {}
+
+reader = csv.DictReader(open('occupations.csv'))
+for row in reader:
+	k, v = row['Job Class'], float(row['Percentage'])
+	occupations_dict[k] = v
 
 app = Flask(__name__)
 
 @app.route('/occupations')
 
 def occupations():
-    return render_template("table.html")
+    return render_template("table.html", foo = occupations_dict)  
+
+def getRandOcc():
+    x = random.uniform(0,99.8)
+    for k in occupations_dict:
+	if (x - (occupations_dict.get(k)) < 0):
+	    return k
+	else:
+	    x = (x - (occupations_dict.get(k)))
 
 if __name__ == '__main__':
     app.debug = True
